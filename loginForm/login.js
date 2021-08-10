d9a9029af1839863c23b7b4d1548a0810d93f77d
Sipos Ferenc <ferenc.m.sipos@gmail.com>
2021-08-10 16:18:22 +0200

    .

--- loginForm/login.js
+++ loginForm/login.js
@@ -3,5 +3,5 @@ var responseDiv = document.getElementById("resp");
 xhttp.onload = function () {
     responseDiv.innerHTML = this.responseText;
 };
-xhttp.open("GET", "https://siposferenc04.atw.hu/index.php");
+xhttp.open("GET", "https://siposferenc04.rf.gd/index.php");
 xhttp.send();
